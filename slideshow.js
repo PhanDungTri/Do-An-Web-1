a670e2beaa600b9861b3a797909ab58732f81a2e
Phan Dung Tri <phandungtri4work@gmail.com>
2018-11-09 14:36:57 +0700

    Add Slide Show to body

---
+++ slideshow.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	var slideIndex = 0;
+	$(".dot").eq(slideIndex).addClass("active");
+	$(".slide").eq(slideIndex).fadeIn(300);
+
+	$(".dot").click(function()
+	{
+		let i = $(".dot").index(this);
+		slideIndex = i;
+		$(this).siblings(".dot").removeClass("active");
+		$(this).addClass("active");
+		$(".slide").eq(i).siblings(".slide").fadeOut(300,function(){
+			$(".slide").eq(i).fadeIn(300);
+		});
+	});
+
+	$(".next").click(function()
+	{
+		moveSlides(1);
+	});
+
+	$(".prev").click(function()
+	{
+		moveSlides(-1);
+	});
+
+	function moveSlides(n)
+	{
+		slideIndex += n;
+		if (slideIndex > $(".dot").last().index()) { slideIndex = 0 }
+		if (slideIndex < 0) { slideIndex = $(".dot").last().index() }
+		$(".dot").eq(slideIndex).addClass("active");
+		$(".dot").eq(slideIndex).siblings(".dot").removeClass("active");
+		$(".slide").eq(slideIndex).siblings(".slide").fadeOut(300,function(){
+			$(".slide").eq(slideIndex).fadeIn(300);
+		});
+	}
+})

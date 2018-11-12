01b5eedd145ee890e36f1a172e729c299011f1ce
Phan Dung Tri <phandungtri4work@gmail.com>
2018-11-12 08:49:07 +0700

    Add Header Menu

--- burger_menu.js
+++ burger_menu.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
+    $(".header-menu").hide();
     $(".burger-menu").click(function () {
         $(this).toggleClass("menu-on");
+        $(".body").toggleClass("blur");
+        $("footer").toggleClass("blur");
+        $(".header-menu").slideToggle();
     });
 });

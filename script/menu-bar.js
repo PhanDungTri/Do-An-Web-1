c4606eafbdf1385da0415b12e8480beab940d4cd
Phan Dung Tri <phandungtri4work@gmail.com>
2018-11-19 10:30:30 +0700

    Fixe menu bar bug

--- script/menu-bar.js
+++ script/menu-bar.js
@@ -2,15 +2,20 @@ $(document).ready(function () {
     var prevPos = $(window).scrollTop();
     $(window).scroll(function () { 
         var currentPos = $(window).scrollTop();
-        if (currentPos < 50)
-            return;
-        if (prevPos > currentPos)
-        {
-            $(".menu-bar").slideDown("fast");
-            $(".menu-bar").css("display", "grid")
+        if ($(window).width() >= 1024) {
+            if (currentPos < 50)
+                return;
+            if (prevPos > currentPos)
+            {
+                $(".menu-bar").slideDown("fast");
+                $(".menu-bar").css("display", "grid")
+            }
+            else
+            {
+                $(".menu-bar").slideUp("fast");
+            }
         }
-        else
-        {
+        else{
             $(".menu-bar").slideUp("fast");
         }
         

0615e279b54f5c8779b515d2ad94a1ec7ebf4e9e
Phan Dung Tri <phandungtri4work@gmail.com>
2018-12-14 21:22:11 +0700

    Login

--- script/login.js
+++ script/login.js
@@ -3,10 +3,12 @@ $(document).ready(() => {
     obj = JSON.parse(text);
     if (obj == null || obj.isLogin == false) {
         $(".login-btn").html("<i class=\"fa fa-user\"></i> sign in");
+        $("#login-btn").text("login");
     }
     else
     {
         $(".login-btn").html("<i class=\"fa fa-user\"></i>" + obj.name);
+        $("#login-btn").text(obj.name);
     }
 
     $(".login-btn").click(() => {

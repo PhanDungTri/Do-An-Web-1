99b9c88422e816e39179d78de1b6be4b45eae502
Phan Dung Tri <phandungtri4work@gmail.com>
2018-12-16 11:22:54 +0700

    Add customer management

--- admin/script/noti.js
+++ admin/script/noti.js
@@ -1,11 +1,11 @@
 $(document).ready(() => {
-    $(".submit-btn").click(() => {
+    $("#remove-btn").click(() => {
         var r = confirm("Do you want to remove the product?");
         if (r) {
             alert("Product removed!");
         }
     });
-    $(".button").click(() => {
+    $("#save-btn").click(() => {
         var r = confirm("Do you want to save the changes?");
         if (r) {
             alert("Information saved!");
